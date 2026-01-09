4ccbf66a035e61eb4bd3348612c1893125efa6ea
Logan-Williams01 <logan.maxwell2001@gmail.com>
2026-01-08 20:05:34 -0600

    Added documentation

--- ItemDialog.py
+++ ItemDialog.py
@@ -8,12 +8,14 @@ from PySide6.QtCore import Qt
 class ItemDialog(QDialog):
     def __init__(self, parent=None, title="Add Item", default_name="", default_id="", default_value=0, editing=False):
         super().__init__(parent)
+        
+        # Sets whether the window should be pre-populated with an existing item or not (defaults to no)
         self.setWindowTitle(title)
         self.editing = editing
 
         layout = QVBoxLayout()
 
-        #Name field
+        #Name field (also calls to auto-update id field)
         name_layout = QHBoxLayout()
         name_layout.addWidget(QLabel("Name:"))
         self.name_input = QLineEdit(default_name)
@@ -21,7 +23,7 @@ class ItemDialog(QDialog):
         name_layout.addWidget(self.name_input)
         layout.addLayout(name_layout)
 
-        #ID field
+        #ID field (cannot be manually edited)
         id_layout = QHBoxLayout()
         id_layout.addWidget(QLabel("Item ID:"))
         self.id_input = QLineEdit(default_id)
@@ -29,10 +31,12 @@ class ItemDialog(QDialog):
         id_layout.addWidget(self.id_input)
         layout.addLayout(id_layout)
 
+        # Let the user know that existing ids cannot be changed
         if editing:
             note = QLabel("Item ID cannot be changed")
             layout.addWidget(note)
 
+        # Sell_value field
         value_layout = QHBoxLayout()
         value_layout.addWidget(QLabel("Sell Value:"))
         self.value_input = QSpinBox()
@@ -42,6 +46,7 @@ class ItemDialog(QDialog):
         value_layout.addWidget(self.value_input)
         layout.addLayout(value_layout)
 
+        # Save and cancel butttons
         btn_layout = QHBoxLayout()
         ok_btn = QPushButton("OK")
         ok_btn.clicked.connect(self.accept)
@@ -51,18 +56,24 @@ class ItemDialog(QDialog):
         btn_layout.addWidget(cancel_btn)
         layout.addLayout(btn_layout)
 
-
         self.setLayout(layout)
 
+        # Only changes id when creating new Item
         if not editing:
             self.update_id_field()
 
     def update_id_field(self):
+
+        # Double check editing mode
         if not self.editing:
             self.id_input.setText(slugify(self.name_input.text()))
     
     def get_data(self):
+
+        # Return all 3 fields
         return self.name_input.text(), self.id_input.text(), self.value_input.value()
 
 def slugify(text: str) ->str:
+
+    # Iron Ore -> iron_ore
     return text.lower().replace(" ", "_")
--- RecipeDialog.py
+++ RecipeDialog.py
@@ -10,62 +10,66 @@ class RecipeDialog(QDialog):
         super().__init__(parent)
         self.setWindowTitle(title)
 
+        # Recieves a list of current Items, and possibly a Recipe to pre-populate from
         self.items = items
         self.recipe = recipe
 
-       
-
         main_layout = QVBoxLayout(self)
 
-
+        # Main layout for adding input rows
         main_layout.addWidget(QLabel("Inputs"))
-
         self.inputs_layout = QVBoxLayout()
         main_layout.addLayout(self.inputs_layout)
 
+        # Button to add an IngredientRow as an input, and a list to maintain them
         self.add_input_btn = QPushButton("+ Add Input")
         main_layout.addWidget(self.add_input_btn)
         self.add_input_btn.clicked.connect(self.add_input_row)
         self.input_rows = []
 
+        # Main layout for adding output rows
         main_layout.addWidget(QLabel("Outputs"))
-
         self.outputs_layout = QVBoxLayout()
         main_layout.addLayout(self.outputs_layout)
 
+        # Button to add an IngredientRow as an output, and a list to maintain them
         self.add_outputs_btn = QPushButton("+ Add Output")
         main_layout.addWidget(self.add_outputs_btn)
         self.add_outputs_btn.clicked.connect(self.add_output_row)
         self.output_rows = []
 
+        # Line for editing recipe type
         form = QFormLayout()
         self.type_edit = QLineEdit("CRAFT")
+
+        # Line for editing recipe time
         self.time_spin = QSpinBox()
         self.time_spin.setMinimum(0)
 
+        # Add both as rows
         form.addRow("Type:", self.type_edit)
         form.addRow("Time:", self.time_spin)
 
         main_layout.addLayout(form)
 
+        # Create and add Save and Cancel buttons
         btn_layout = QHBoxLayout()
         self.save_btn = QPushButton("Save")
         self.cancel_btn = QPushButton("Cancel")
-
-        btn_layout.addStretch()
         btn_layout.addWidget(self.save_btn)
         btn_layout.addWidget(self.cancel_btn)
-
-        main_layout.addLayout(btn_layout)
-
         self.save_btn.clicked.connect(self.on_save)
         self.cancel_btn.clicked.connect(self.reject)
+        btn_layout.addStretch()
+        main_layout.addLayout(btn_layout)
 
+        # If passed an existing recipe, populate fields
         if self.recipe:
             self.populate_from_recipe()
 
-
     def add_input_row(self):
+
+        # Create an Ingredient row with existing items and set RecipeDialog as parent, and connect remove button
         row = IngredientRow(self.items, parent=self)
         row.remove_requested.connect(self.remove_input_row)
         self.inputs_layout.addWidget(row)
@@ -73,6 +77,8 @@ class RecipeDialog(QDialog):
         return row
     
     def add_output_row(self):
+
+        # Create an Ingredient row with existing items and set RecipeDialog as parent, and connect remove button
         row = IngredientRow(self.items, parent=self)
         row.remove_requested.connect(self.remove_output_row)
         self.outputs_layout.addWidget(row)
@@ -80,36 +86,48 @@ class RecipeDialog(QDialog):
         return row
 
     def remove_input_row(self, row:IngredientRow):
+
+        # Remove the row from the RecipeDialog's list of input rows, and delete the IngredientRow
         if row in self.input_rows:
             self.input_rows.remove(row)
             row.setParent(None)
             row.deleteLater()
 
     def remove_output_row(self, row):
+
+        # Remove the row from the RecipeDialog's list of output rows, and delete the IngredientRow
         if row in self.output_rows:
             self.output_rows.remove(row)
             row.setParent(None)
             row.deleteLater()
     
     def populate_from_recipe(self):
+
+        # Sanity check
         if not self.recipe:
             return
         
+        # Create an input row for each item in the Recipe's inputs
         for item_id, qty in self.recipe.inputs.items():
             row = self.add_input_row()
             row.item_combo.setCurrentIndex(row.item_combo.findData(item_id))
             row.qty_spin.setValue(qty)
 
+        # Create an output row for each item in the Recipe's outputs
         for item_id, qty in self.recipe.outputs.items():
             row = self.add_output_row()
             row.item_combo.setCurrentIndex(row.item_combo.findData(item_id))
             row.qty_spin.setValue(qty)
 
+        # Set type and time
         self.type_edit.setText(self.recipe.type)
         self.time_spin.setValue(int(self.recipe.time))
 
-
     def on_save(self):
+        
+        # Attempt to create a Recipe 
+
+        # Collect inputs as a dictionary for Recipe's constructor
         inputs = {}
         row:IngredientRow
         for row in self.input_rows:
@@ -127,6 +145,7 @@ class RecipeDialog(QDialog):
                 return
             inputs[item_id] = quantity
 
+        # Collect outputs as a dictionary for Recipe's constructor
         outputs = {}
         row:IngredientRow
         for row in self.output_rows:
@@ -144,33 +163,41 @@ class RecipeDialog(QDialog):
                 return
             outputs[item_id] = quantity
         
+        # Ensure both inputs and outputs have items
         if not inputs or not outputs:
             QMessageBox.warning(self, "Invalid recipe", "A recipe must have at least one input and one output.")
             return
 
+        # Set type and time
         recipe_type = self.type_edit.text()
         recipe_time = self.time_spin.value()
 
+        # Create the Recipe or display a warning
         try:
             recipe = Recipe(inputs, outputs, type=recipe_type, time=recipe_time)
         except Exception as e:
             QMessageBox.warning(self, "Invalid recipe", str(e))
             return
 
+        # Set recipe and close dialog
         self.recipe = recipe
         self.accept()
 
+# Custom class used to create a combo box with a list of existing Items, a field for quantity, and a button to delete itself
 class IngredientRow(QWidget):
     
+    # Signal used for deleting self
     remove_requested = Signal(object)
 
     def __init__(self, items: dict[str, Item], parent=None):
         super().__init__(parent)
 
+        # Must be passed a dictionary of current items
         self.items = items
 
         layout = QHBoxLayout(self)
 
+        # Typable ComboBox with filter for finding items easily
         self.item_combo = QComboBox()
         self.item_combo.setEditable(True)
         self.item_combo.setInsertPolicy(QComboBox.NoInsert)
@@ -178,33 +205,40 @@ class IngredientRow(QWidget):
         self.item_combo.completer().setFilterMode(Qt.MatchContains)
         self.item_combo.addItem("Select item...", None)
         self.line_edit = self.item_combo.lineEdit()
+
+        # Also has the capability of auto-focusing itself and selecting all text for convenience
         self.line_edit.installEventFilter(self)
 
+        # Formatting
         self.item_combo.setMinimumHeight(self.item_combo.fontMetrics().height() + 7)
 
+        # Loop through items list and add them to the drop-down
         for item_id, item in sorted(items.items(), key=lambda kv: kv[1].name.lower()):
             self.item_combo.addItem(item.name, item_id)
 
-
-
+        # Quantity selector
         self.qty_spin = QSpinBox()
         self.qty_spin.setMinimum(1)
         self.qty_spin.setMaximum(1_000_000_000)
         self.qty_spin.setValue(1)
 
+        # Delete row button
         self.remove_btn = QPushButton("X")
         self.remove_btn.setFixedWidth(28)
+        self.remove_btn.clicked.connect(self.on_remove)
 
+        # Add the widgets in order
         layout.addWidget(self.item_combo)
         layout.addWidget(self.qty_spin)
         layout.addWidget(self.remove_btn)
 
+        # IngredientRows are used elsewhere, so only auto-focus in RecipeDialogs
         if type(parent) == RecipeDialog:
             QTimer.singleShot(0, lambda: self._focus_combo())
 
-        self.remove_btn.clicked.connect(self.on_remove)
-    
     def refresh_items(self, items:dict[str, Item]):
+        
+        # Copies the logic for adding items in __init__, specifically for the modified IngredientRow used in the Recipe tab's filter bar
         self.items = items
         self.item_combo.clear()
         self.item_combo.addItem("Select item...", None)
@@ -212,16 +246,21 @@ class IngredientRow(QWidget):
         for item_id, item in sorted(items.items(), key=lambda kv: kv[1].name.lower()):
             self.item_combo.addItem(item.name, item_id)
 
-
     def _focus_combo(self):
+
+        # Focuses itself and selects all text
         self.item_combo.setFocus()
         self.item_combo.lineEdit().selectAll()
 
     def eventFilter(self, obj, event):
+
+        # Selects all text on-clcik
         if obj == self.item_combo.lineEdit():
             if event.type() ==QEvent.MouseButtonPress:
                 QTimer.singleShot(0, obj.selectAll)
         return super().eventFilter(obj, event)
 
     def on_remove(self):
+
+        # Removes itself and sends signal to parent to remove child reference
         self.remove_requested.emit(self)
